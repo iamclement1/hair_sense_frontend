c17968a44d0fac48d6ce3248a207ae93c6a9f1e1
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2023-04-27 13:23:15 +0100

    update: Add ProductSlider Import

--- components/Common/index.js
+++ components/Common/index.js
@@ -4,3 +4,4 @@ export { default as HeroSlider } from "./HeroSlider";
 export { default as Navbar } from "./Navbar";
 export { default as NavDropDown } from "./NavDropDown";
 export { default as SearchInput } from "./SearchInput";
+export { default as ProductSlider } from "./ProductSlider";
