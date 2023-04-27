58afaf4d4916881ef0e350c5aef2c21122e98670
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2023-04-27 13:23:52 +0100

    Update: Add new shades

--- utils/theme.js
+++ utils/theme.js
@@ -1,6 +1,5 @@
 import { extendTheme } from "@chakra-ui/react";
 
-
 // Extending the theme to include custom colors, fonts, etc
 const colors = {
     // Primary Color
@@ -11,8 +10,8 @@ const colors = {
     shades_3: "#93A4E2",
     shades_4: "#637ACD",
     shades_5: "#263F99",
-    shades_6:"#7A7474",
-    shades_7:"#959595",
+    shades_6: "#7A7474",
+    shades_7: "#959595",
     // Secondary Color
     secondary_1: "#F3F3F3",
     // Accent Colors
