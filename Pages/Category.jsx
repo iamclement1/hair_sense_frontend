bb522d6bb3bc530f554ee3d5df17294017c0f3dc
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2023-04-30 11:02:46 +0100

    Update: remove Navbar Conditional rendering

---
+++ Pages/Category.jsx
@@ -0,0 +1,14 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+const Category = ({ data }) => {
+    return (
+        <Box mt={["40px", null, "100px"]}>
+            <Box>
+                
+            </Box>
+        </Box>
+    );
+};
+
+export default Category;
