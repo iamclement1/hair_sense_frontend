c4b3be2e82a6a0ff902071a60059025bb3ff9383
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2023-04-30 10:56:08 +0100

    Feat: Social Buttons

--- components/Common/Button.jsx
+++ components/Common/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Button } from "@chakra-ui/react";
+import { Button, Icon, Image, Text } from "@chakra-ui/react";
 
-const PrimaryButton = ({ text, ...props }) => {
+export const PrimaryButton = ({ text, ...props }) => {
     return (
         <Button
             {...props}
@@ -9,10 +9,61 @@ const PrimaryButton = ({ text, ...props }) => {
             _active={{}}
             _focus={{}}
             bgColor={"primary_1"}
+            color={"white"}
+            px={["16px", null, "26px"]}
+            py="15px"
+            display={"block"}
+            h="auto"
+            w="100%"
+            fontSize={["14px", null, "20px", null, "20px"]}
         >
             {text}
         </Button>
     );
 };
 
-export default PrimaryButton;
+export const SocialButton = ({ icon, text, imageUrl, imageText, ...props }) => {
+    return (
+        <Button
+            display="flex"
+            alignContent={"center"}
+            justifyContent={"center"}
+            {...props}
+            _hover={{}}
+            _active={{}}
+            _focus={{}}
+            bgColor={"transparent"}
+            px={["16px", null, "26px"]}
+            py="15px"
+            h="auto"
+            w="100%"
+            fontSize={["14px", null, "20px", null, "20px"]}
+            boxShadow={"0px 1.466px 1.466px rgba(51, 154, 240, 0.1)"}
+            border={"1px"}
+            borderColor={"dark_2"}
+            gap={["12px", "24px"]}
+        >
+            <Text textAlign={"right"} flex={1}>
+                {icon && <Icon as={icon} boxSize={["25px"]} />}
+                {imageUrl && (
+                    <Image
+                        src={imageUrl}
+                        alt={imageText}
+                        display="inline-block"
+                        w={["24px", null, "35px"]}
+                    />
+                )}
+            </Text>
+            <Text
+                w={["80%",null, null, null,"70%","60%"]}
+                color="dark_3"
+                fontWeight={400}
+                fontSize={["16px", null, "20px"]}
+                textAlign={"left"}
+            >
+                {" "}
+                {text}
+            </Text>
+        </Button>
+    );
+};
