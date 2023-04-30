0d7af1177d40cf4bb9cf693a09c1822a3d9eee87
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2023-04-30 11:05:01 +0100

    Update: Change Link to Router

--- components/Common/Navbar.jsx
+++ components/Common/Navbar.jsx
@@ -21,109 +21,142 @@ import {
 } from "react-icons/fa";
 import { NavDropDown } from ".";
 import MobileNav from "./MobileNav";
+import AuthModal from "../modal/AuthModal";
 
 const Navbar = () => {
+    // fuction to Open Nav
     const { isOpen, onOpen, onClose } = useDisclosure();
+    // function to handle Authication  modal
+    const {
+        isOpen: isOpenAuth,
+        onOpen: onOpenAuth,
+        onClose: onCloseAuth,
+    } = useDisclosure();
+    // function to handle Cart  modal
+    const {
+        isOpen: isOpenCart,
+        onOpen: onOpenCart,
+        onClose: onCloseCart,
+    } = useDisclosure();
+
+    const navigateToFav = () => {
+        router.push("/");
+    };
+    const menuItems = [
+        {
+            url: () => onOpenAuth(),
+            text: "My Account",
+            icon: FaUser,
+        },
+        {
+            url: navigateToFav,
+            text: "Favorite product",
+            icon: FaHeart,
+        },
+        {
+            url: onOpenCart,
+            text: "My Cart",
+            icon: FaShoppingCart,
+        },
+    ];
 
     return (
-        <Box bgColor="secondary_1">
-            <ScreenSize>
-                <Flex
-                    align="center"
-                    justify="space-between"
-                    py="40px"
-                    gap="20px"
-                    flexWrap={["wrap", null, "nowrap"]}
-                >
-                    {/* The logo section */}
+        <>
+            <Box bgColor="secondary_1">
+                <ScreenSize>
+                    <Flex
+                        align="center"
+                        justify="space-between"
+                        py="40px"
+                        gap="20px"
+                        flexWrap={["wrap", null, "nowrap"]}
+                    >
+                        {/* The logo section */}
 
-                    <Box display="flex" alignItems="center" flexShrink={"0"}>
-                        <Link as={NextLink} href="/">
-                            <Image
-                                src="/images/logo.svg"
-                                alt="Logo"
-                                height="100%"
-                                width={["120px", "150px", "150px", "200px"]}
-                                mr={2}
-                            />
-                        </Link>
-                    </Box>
+                        <Box
+                            display="flex"
+                            alignItems="center"
+                            flexShrink={"0"}
+                        >
+                            <Link as={NextLink} href="/">
+                                <Image
+                                    src="/images/logo.svg"
+                                    alt="Logo"
+                                    height="100%"
+                                    width={["120px", "150px", "150px", "200px"]}
+                                    mr={2}
+                                />
+                            </Link>
+                        </Box>
 
-                    {/*Search Input Section */}
-                    <Box
-                        w={["100%", null, "100%"]}
-                        order={[3, 3, 2]}
-                        // flexShrink={1}
-                    >
-                        <SearchInput />
+                        {/*Search Input Section */}
+                        <Box
+                            w={["100%", null, "100%"]}
+                            order={[3, 3, 2]}
+                            // flexShrink={1}
+                        >
+                            <SearchInput />
+                        </Box>
+                        {/* Account and other menu section */}
+                        <Box
+                            order={3}
+                            justifyContent={"flex-end"}
+                            display={["none", "none", "none", "flex"]}
+                            flexShrink={0}
+                        >
+                            <Flex gap="20px">
+                                {menuItems.map(({ url, text, icon }, i) => {
+                                    return (
+                                        <Box
+                                            cursor={"pointer"}
+                                            key={i}
+                                            textAlign={"center"}
+                                            onClick={url}
+                                        >
+                                            <Icon as={icon} color="accent_2" />
+                                            <Text color="accent_2">
+                                                {text}{" "}
+                                            </Text>
+                                        </Box>
+                                    );
+                                })}
+                            </Flex>
+                        </Box>
+                        {/* Humbuger for Mobile Nav */}
+                        <Box
+                            // w={"50%"}
+                            order={[2, 2, 3]}
+                            display={["block", null, "null", "none"]}
+                            onClick={onOpen}
+                            cursor={"pointer"}
+                        >
+                            <Icon as={FaBars} boxSize={"25px"} />
+                        </Box>
+                    </Flex>
+                    {/* Destop NaV */}
+                    <Box>
+                        <NavDropDown />
                     </Box>
-                    {/* Account and other menu section */}
-                    <Box
-                        order={3}
-                        justifyContent={"flex-end"}
-                        display={["none", "none", "none", "flex"]}
-                        flexShrink={0}
-                    >
-                        <Flex gap="20px">
-                            {menuItems.map(({ url, text, icon }, i) => {
-                                return (
-                                    <Link
-                                        key={i}
-                                        as={NextLink}
-                                        href={url}
-                                        textAlign={"center"}
-                                    >
-                                        <Icon as={icon} color="accent_2" />
-                                        <Text color="shades_6">{text} </Text>
-                                    </Link>
-                                );
-                            })}
-                        </Flex>
+                    {/* Moble Nav */}
+                    <Box>
+                        <MobileNav
+                            isOpen={isOpen}
+                            onOpen={onOpen}
+                            onClose={onClose}
+                        />
                     </Box>
-                    {/* Humbuger for Mobile Nav */}
-                    <Box
-                        // w={"50%"}
-                        order={[2, 2, 3]}
-                        display={["block", null, "null", "none"]}
-                        onClick={onOpen}
-                        cursor={"pointer"}
-                    >
-                        <Icon as={FaBars} boxSize={"25px"} />
-                    </Box>
-                </Flex>
-                {/* Destop NaV */}
-                <Box>
-                    <NavDropDown />
-                </Box>
-                {/* Moble Nav */}
-                <Box>
-                    <MobileNav
-                        isOpen={isOpen}
-                        onOpen={onOpen}
-                        onClose={onClose}
-                    />
-                </Box>
-            </ScreenSize>
-        </Box>
+                </ScreenSize>
+
+                {/* Modal Requirement Importation for the Account */}
+            </Box>
+
+            <AuthModal
+                isOpen={isOpenAuth}
+                onOpen={onOpenAuth}
+                onClose={onCloseAuth}
+            />
+        </>
     );
 };
 
 export default Navbar;
-
-const menuItems = [
-    {
-        url: "/",
-        text: "My Account",
-        icon: FaUser,
-    },
-    {
-        url: "/",
-        text: "Favorite product",
-        icon: FaHeart,
-    },
-    {
-        url: "/",
-        text: "My Cart",
-        icon: FaShoppingCart,
-    },
-];
