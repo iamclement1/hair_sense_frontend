b6e27b5f15e609b1b5a9fbe2765292c56854945d
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2023-04-30 11:00:29 +0100

    Update: Default Export

--- components/Common/index.js
+++ components/Common/index.js
@@ -1,4 +1,4 @@
-export { default as PrimaryButton } from "./Button";
+export { PrimaryButton } from "./Button";
 export { default as Footer } from "./Footer";
 export { default as HeroSlider } from "./HeroSlider";
 export { default as Navbar } from "./Navbar";
