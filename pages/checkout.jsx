5d05968a534eacb7636e8add14f2558e72e7744a
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2023-05-22 09:26:04 +0100

    Chaecout page flow

---
+++ pages/checkout.jsx
@@ -0,0 +1,12 @@
+import CheckoutPage from '@/My-pages/CheckoutPage'
+import React from 'react'
+
+const checkout = () => {
+  return (
+    <div>
+        <CheckoutPage />
+    </div>
+  )
+}
+
+export default checkout
