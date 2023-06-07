286c94259a2f58b38fb5841c9c8b227afb052caf
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2023-06-07 10:53:12 +0100

    "feats: added table pagination"

--- components/Admin/Products.jsx
+++ components/Admin/Products.jsx
@@ -1,13 +1,35 @@
 import { baseUrl, httpGet } from '@/http-request/http-request';
-import { Button, Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
+import { Flex, Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
 import Cookies from 'js-cookie';
 import React, { useEffect, useMemo, useState } from 'react'
+import { AiFillEdit } from 'react-icons/ai';
+import { MdDelete } from 'react-icons/md';
 import { useTable } from 'react-table';
+import TablePagination from '../Common/TablePagination';
 
 const Products = () => {
    const [products, setProducts] = useState(null);
-
+   const [currentPage, setCurrentPage] = useState(1);
    const accessToken = Cookies.get('access_token');
+
+
+   //pagination session starts here
+   const itemsPerPage = 10;
+
+   //calculate the total number of page
+   const totalPages = Math.ceil(products && products.length / itemsPerPage);
+
+   const handlePageChange = (selectedPage) => {
+      setCurrentPage(selectedPage);
+   }
+
+   //the calculation of the start and end index per page starts here
+   const startIndex = (currentPage - 1) * itemsPerPage;
+   const endIndex = startIndex + itemsPerPage;
+   const itemsToDisplay = products && products.length > 0 && products.slice(startIndex, endIndex);
+   // console.log(itemsToDisplay);
+
+   //pagination session ends here
 
    useEffect(() => {
       async function fetchProduct() {
@@ -54,12 +76,12 @@ const Products = () => {
       }
    ], [])
 
-   const EnvData = useMemo(() => products, [products])
+   const EnvData = useMemo(() => itemsToDisplay, [itemsToDisplay])
 
    const EnvColumn = useMemo(() => {
       if (products && products.length > 0) {
          const validKeys = Object.keys(products[0]).filter(
-            key => !["comment", "rate", "commenter", "product", "views"].includes(key)
+            key => !["comment", "rate", "commenter", "second_description", "sales_price", "product", "views", "product_img"].includes(key)
          );
          return validKeys.map(key => ({
             Header: key,
@@ -73,6 +95,19 @@ const Products = () => {
 
    const tableInstance = useTable({ columns: EnvColumn, data: EnvData });
    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = tableInstance;
+
+
+   // Handler for the edit action
+   const handleEdit = (productId) => {
+      // Implement the logic for handling the edit action
+      console.log("Edit product with ID:", productId);
+   };
+
+   // Handler for the delete action
+   const handleDelete = (productId) => {
+      // Implement the logic for handling the delete action
+      console.log("Delete product with ID:", productId);
+   };
 
    return (
       <React.Fragment>
@@ -94,6 +129,8 @@ const Products = () => {
                <Tbody {...getTableBodyProps()}>
                   {rows.map(row => {
                      prepareRow(row);
+                     const { id } = row.original; // Get the ID of the product
+
                      return (
                         <Tr {...row.getRowProps()} key={row.id}>
                            {row.cells.map(cell => (
@@ -102,9 +139,18 @@ const Products = () => {
                               </Td>
                            ))}
                            <Td>
-                              <Button colorScheme="blue" size="sm" mr={2}>
-                                 Edit
-                              </Button>
+                              <Flex>
+                                 <AiFillEdit color="blue"
+                                    // size="xs"
+                                    mr={2}
+                                    cursor={"pointer"}
+                                    onClick={() => handleEdit(id)} />
+                                 <MdDelete
+                                    color="red"
+                                    // size="xs"
+                                    cursor={"pointer"}
+                                    onClick={() => handleDelete(id)} />
+                              </Flex>
                            </Td>
                         </Tr>
                      );
@@ -113,6 +159,10 @@ const Products = () => {
             </Table>
          ) : (
             <div></div>
+         )}
+
+         {totalPages > 1 && (
+            <TablePagination pageCount={totalPages} onPageChange={handlePageChange} />
          )}
       </React.Fragment>
 
