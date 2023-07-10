4142456307aee9818d15457749bbc96dfbce262d
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2023-07-10 22:39:27 +0100

    feat: productione env :bug:

--- http-request/http-request.jsx
+++ http-request/http-request.jsx
@@ -4,11 +4,11 @@ import { toast } from "react-toastify";
 import { ERROR_RESPONSES } from "./response";
 
 // export const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-export const baseUrl =
-    process.env.NEXT_PUBLIC_ENVIRONMENT === "production"
-        ? process.env.NEXT_PUBLIC_BASE_URL
-        : process.env.NEXT_PUBLIC_BASE_URL;
-// export const baseUrl = "https://backend.hairsenseretail.com"
+// export const baseUrl =
+//     process.env.NEXT_PUBLIC_ENVIRONMENT === "production"
+//         ? process.env.NEXT_PUBLIC_BASE_URL
+//         : process.env.NEXT_PUBLIC_BASE_URL;
+export const baseUrl = "https://backend.hairsenseretail.com"
 // console.log("Base Ur === ", baseUrl);
 
 // console.log(baseUrl);
