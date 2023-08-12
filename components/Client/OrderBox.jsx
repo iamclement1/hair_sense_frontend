24194ac98fbd0f1cffd07db44ad7f27c0a227cf4
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2023-08-12 09:37:00 +0100

    payment update fix

---
+++ components/Client/OrderBox.jsx
@@ -0,0 +1,113 @@
+import { PrimaryButton, ProductSlider } from "@/components/Common";
+import Badge from "@/components/Common/Badge";
+import {
+    Box,
+    Divider,
+    Flex,
+    Text,
+    Icon,
+    Image,
+    useDisclosure,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { FiChevronLeft } from "react-icons/fi";
+import { IoIosCloseCircleOutline } from "react-icons/io";
+
+const OrderBox = ({ data }) => {
+    console.log("dada", data);
+    return (
+        <Box>
+            <Flex
+                flexDirection="column"
+                justifyContent="space-between"
+                py="30px"
+            >
+                <Flex w="100%" h="max-content" gap={["none", "30px"]}>
+                    <Box>
+                        <Image
+                            // src={product?.product_img}
+                            // alt={product?.name}
+
+                            src="/images/cream.svg"
+                            alt={"dummy "}
+                            w={{
+                                base: "125px",
+                                md: "130px",
+                            }}
+                            h={{
+                                base: "115px",
+                                md: "130px",
+                            }}
+                            objectFit="cover"
+                        />
+                    </Box>
+                    <Flex flexDir="column" justify="space-between" flex="1">
+                        <Box>
+                            <Text
+                                fontSize={{
+                                    base: "12px",
+                                    sm: "14px",
+                                    md: "18px",
+                                    // lg: "23px",
+                                }}
+                                fontWeight={"600"}
+                                maxW="600px"
+                                noOfLines={2}
+                            >
+                                {/* {product?.name} */}
+                                Hyggee Vegan Sun Cream - 50ml
+                            </Text>
+                        </Box>
+                        <Flex
+                            justify="space-between"
+                            align={["unset", null, "center"]}
+                            gap="16px"
+                            flexDir={["column", null, "row"]}
+                            order={{ base: "3", md: "2", xl: "2" }}
+                        >
+                            <Box>
+                                <Text
+                                    fontSize={{
+                                        base: "14px",
+                                        md: "18px",
+                                        // lg: "23px",
+                                    }}
+                                    fontWeight={"600"}
+                                >
+                                    ₦ 4,000
+                                </Text>
+                            </Box>
+
+                            {/* <PrimaryButton
+                                maxW="fit-content"
+                                text="Track Order"
+                                handleButton={() =>
+                                    router.push(`/track_orders/${"Product Id"}`)
+                                }
+                            /> */}
+
+                            <Badge />
+                        </Flex>
+                        <Box order={{ base: "2", md: "2", xl: "2" }}>
+                            <Text
+                                fontSize={{
+                                    base: "14px",
+                                    md: "18px",
+                                    // lg: "23px",
+                                }}
+                                fontWeight={"400"}
+                                maxW="600px"
+                                noOfLines={2}
+                            >
+                                {/* {product?.name} */}
+                                22/04/23
+                            </Text>
+                        </Box>
+                    </Flex>
+                </Flex>
+            </Flex>
+        </Box>
+    );
+};
+
+export default OrderBox;
