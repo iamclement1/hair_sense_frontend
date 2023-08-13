4cdb53ac26b991b3f5f34d3473184ef3b1a95b55
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2023-08-13 20:00:05 +0100

    update page authentication

--- pages/my_account.jsx
+++ pages/my_account.jsx
@@ -1,7 +1,7 @@
-
 import MyAccount from "@/My-pages/MyAccount";
 import { Layout, ScreenSize } from "@/components/layouts";
 import { StateContext } from "@/context/StateProvider";
+import Cookies from "js-cookie";
 import { useRouter } from "next/router";
 import React, { useContext, useEffect } from "react";
 
@@ -9,10 +9,11 @@ const my_account = () => {
     const router = useRouter();
     const { user } = useContext(StateContext);
     useEffect(() => {
-        if (!user) {
-            router.push('/')
+        const isLoggedIn = Cookies.get("access_token");
+        if (!isLoggedIn) {
+            router.push("/");
         }
-    })
+    }, []);
     return (
         <Layout>
             <ScreenSize>
