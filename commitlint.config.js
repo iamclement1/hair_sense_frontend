586797be237372471d86b785ff14527c55e563ad
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2024-01-23 23:26:22 +0100

    husky

---
+++ commitlint.config.js
@@ -0,0 +1,18 @@
+import { types, scopes } from "./conventionalCommit.json";
+
+const typesEnum = Object.keys(types);
+const scopesEnum = Object.keys(scopes);
+
+const config = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [2, "always", typesEnum],
+    "scope-case": [2, "always", ["camel-case"]],
+    "scope-enum": [2, "always", scopesEnum],
+    "subject-empty": [2, "never"],
+    "subject-case": [2, "always", ["lower-case"]],
+    "header-max-length": [2, "always", 72],
+  },
+};
+
+export default config;
