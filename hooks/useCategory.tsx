6927f346c7966bdca7142d1a9a9dedba27ec4145
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2024-02-15 08:43:46 +0100

    feat: fetch category :fire:

---
+++ hooks/useCategory.tsx
@@ -0,0 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+import client from "@/context/axiosInstance";
+
+const useCategory = () => {
+  const fetchCategory = async () => {
+    const data = await client.get("/store/products");
+    return data;
+  };
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["category"],
+    queryFn: fetchCategory,
+    staleTime: 300000,
+  });
+
+  return {
+    data,
+    isLoading,
+    error,
+  };
+};
+
+export default useCategory;
