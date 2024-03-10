bbd0e8022dd3d1806f953e17ffbac76b8e970802
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2024-03-10 13:17:00 +0100

    feat: new feature :bulb:

--- My-pages/Category.jsx
+++ My-pages/Category.jsx
@@ -1,30 +1,33 @@
+import { ProductSlider } from "@/components/Common";
 import Pagination from "@/components/Common/Pagination";
 import ProductBox from "@/components/Common/ProductBox";
 
-import { Box, Divider, SimpleGrid, Text } from "@chakra-ui/react";
+import { Box, Divider, Flex, SimpleGrid, Text } from "@chakra-ui/react";
 import React from "react";
 
-const Category = ({ data }) => {
+const Category = ({ data, subCategoryName }) => {
+    const catData = data?.products;
+
     return (
         <Box>
-            <Box
-                mt={["80px", null, null, "100px"]}
-                mb={["40px", null, null, "80px"]}
-            >
+            <Box mt={["80px", null, null, "100px"]} mb={["40px", null, null, "80px"]}>
                 <Text
                     color="accent_2"
                     fontSize={["16px", null, "25px", null, "40px"]}
                     mb="12px"
                     fontWeight={600}
-                    textTransform={"capitalize"}
+                    textTransform="capitalize"
                 >
-                    {data}
+                    {data?.name}
                 </Text>
                 <Divider />
             </Box>
             <Box>
-                
-                <Pagination />
+                {catData?.length > 0 ? (
+                    <ProductSlider productDatas={catData} />
+                ) : (
+                    <Text>No products available for {subCategoryName}</Text>
+                )}
             </Box>
         </Box>
     );
