d6ca67441c86d16b1155023d30785db719d8ceb9
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2024-03-17 17:29:35 +0100

    feat: display all admin :bulb:

--- components/SuperAdmin/SuperDashboardAdmins.jsx
+++ components/SuperAdmin/SuperDashboardAdmins.jsx
@@ -5,13 +5,25 @@ import CustomTable from "./Tables/CustomTable";
 import AdminMode from "./AdminModeModals";
 import { FaUser } from "react-icons/fa";
 import CreateAdmin from "./CreateAdmin";
+import useAdmin from "@/hooks/useAdmin";
+import CustomSpinner from "../Common/Spinner";
+import useDataErrorToast from "@/hooks/useErrorToast";
 
-const SuperDashboardAdmins = () => {
+const SuperDashboardAdmins = ({ setActivePage }) => {
+
+    const { data: adminData, isLoading, error } = useAdmin();
+
+    const allAdmins = (adminData?.data?.data?.data)
+
+    useDataErrorToast(error, adminData, setActivePage)
+
+
+    if (isLoading) return <CustomSpinner />
+
     const columns = [
-        { id: "s/n", Header: "S/N", accessor: (row) => row.id },
-        { id: "name", Header: "Name", accessor: (row) => row.name },
+        { id: "name", Header: "Name", accessor: (row) => `${row.firstName} ${row.lastName}` },
         { id: "email", Header: "Email", accessor: (row) => row.email },
-        { id: "phoneNo", Header: "Phone No", accessor: (row) => row.phoneNo },
+        { id: "phoneNo", Header: "Phone No", accessor: (row) => row.phone },
         {
             id: "",
             Header: "Set Status",
@@ -35,23 +47,8 @@ const SuperDashboardAdmins = () => {
                 </Flex>
             ),
         },
-        // { id: "order_no", Header: "Order no", accessor: (row) => row.order_no },
-        // {
-        //     id: "price",
-        //     Header: "Price",
-        //     accessor: (row) => <Text> ₦{row.price}</Text>,
-        // },
     ];
 
-    const data = [
-        {
-            id: 1,
-            name: "John Doe",
-            date: "03/08/2024",
-            email: "Adesh16@gmail.com",
-            phoneNo: "07030075660",
-        },
-    ];
 
     return (
         <Box>
@@ -72,7 +69,7 @@ const SuperDashboardAdmins = () => {
                 <Text fontSize="20px">A list of all Admins.</Text>
 
                 <Box mt="2.5rem">
-                    <CustomTable data={data} columns={columns} />
+                    <CustomTable data={allAdmins} columns={columns} />
                 </Box>
             </Box>
         </Box>
