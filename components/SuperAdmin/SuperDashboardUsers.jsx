78c117b320f4116dcda030b1054fc6ea8c3b4680
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2024-03-18 20:31:40 +0100

    chore: show all clients :fire:

--- components/SuperAdmin/SuperDashboardUsers.jsx
+++ components/SuperAdmin/SuperDashboardUsers.jsx
@@ -12,16 +12,13 @@ const SuperDashboardUsers = ({ setActivePage }) => {
     useDataErrorToast(error, customerData, setActivePage)
 
     if (isLoading) return <CustomSpinner />
-    console.log(customerData)
 
-
+    const customer = customerData?.data?.data
 
     const columns = [
-        { id: "s/n", Header: "S/N", accessor: (row) => row.id },
-        { id: "name", Header: "Name", accessor: (row) => row.name },
+        { id: "name", Header: "Name", accessor: (row) => `${row.firstName} ${row.lastName}` },
         { id: "email", Header: "Email", accessor: (row) => row.email },
-        { id: "phoneNo", Header: "Phone No", accessor: (row) => row.phoneNo },
-        // { id: "item", Header: "Item", accessor: (row) => row.item },
+        { id: "phoneNo", Header: "Phone No", accessor: (row) => row.phone },
         // { id: "order_no", Header: "Order no", accessor: (row) => row.order_no },
         // {
         //     id: "price",
@@ -87,7 +84,7 @@ const SuperDashboardUsers = ({ setActivePage }) => {
                 <Text fontSize="20px">A list of all Users.</Text>
 
                 <Box mt="2.5rem">
-                    <CustomTable data={data} columns={columns} />
+                    <CustomTable data={customer} columns={columns} />
                 </Box>
             </Box>
         </Box>
