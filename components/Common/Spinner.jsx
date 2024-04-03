4cd20dd1a1550a1c88dabfc13c676c814f8619a0
umarfaruq43 <abdulazeezfaruq43@gmail.com>
2024-04-03 11:05:32 +0100

    refactor: increase spinner zindex

--- components/Common/Spinner.jsx
+++ components/Common/Spinner.jsx
@@ -1,21 +1,22 @@
 import { Flex, Spinner } from "@chakra-ui/react";
 
 const CustomSpinner = () => {
-   return (
-      <Flex
-         align="center"
-         justify="center"
-         height="100vh"
-         position="fixed"
-         top="0"
-         left="0"
-         right="0"
-         bottom="0"
-         backgroundColor="rgba(255, 255, 255, 0.7)" // Optional: Add a semi-transparent background color
-      >
-         <Spinner color="blue.600" size="lg" />
-      </Flex>
-   );
+    return (
+        <Flex
+            align="center"
+            justify="center"
+            height="100vh"
+            position="fixed"
+            top="0"
+            left="0"
+            right="0"
+            bottom="0"
+            zIndex={2000}
+            backgroundColor="rgba(255, 255, 255, 0.7)" // Optional: Add a semi-transparent background color
+        >
+            <Spinner color="blue.600" size="lg" />
+        </Flex>
+    );
 };
 
 export default CustomSpinner;
